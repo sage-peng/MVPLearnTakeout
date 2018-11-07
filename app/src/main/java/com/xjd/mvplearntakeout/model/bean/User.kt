f80c594a508620538e3e249f27333bd4807fd657
sage peng <56102515@qq.com>
2018-11-07 23:45:50 +0800

    - 集成ormlite

--- app/src/main/java/com/xjd/mvplearntakeout/model/bean/User.kt
+++ app/src/main/java/com/xjd/mvplearntakeout/model/bean/User.kt
@@ -1,13 +1,16 @@
 package com.xjd.mvplearntakeout.model.bean
 
-class User {
+import com.google.gson.FieldNamingPolicy
+import com.j256.ormlite.field.DatabaseField
+import com.j256.ormlite.table.DatabaseTable
 
-    var id: Int = 0
-    var name: String? = null
-    var balance: Float = 0.toFloat()
-    var discount: Int = 0
-    var integral: Int = 0
-    var phone: String? = null
+@DatabaseTable class User {
 
+    @DatabaseField(id=true) var id: Int = 0
+    @DatabaseField(columnName = "name") var name: String? = null
+    @DatabaseField(columnName = "balance") var balance: Float = 0.toFloat()
+    @DatabaseField(columnName = "discount") var discount: Int = 0
+    @DatabaseField(columnName = "integral") var integral: Int = 0
+    @DatabaseField(columnName = "phone") var phone: String? = null
 
 }
